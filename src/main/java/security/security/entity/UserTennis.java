b33526f6e4cb06f6c59ab7baedcd4e03f006dd00
François Zuliani <zulianifrancois@yahoo.fr>
2024-08-28 19:57:57 +0200

    Initial commit

---
+++ src/main/java/security/security/entity/UserTennis.java
@@ -0,0 +1,35 @@
+package security.security.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_tennis")
+public class UserTennis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 64)
+    private String username;
+
+    @Column(nullable = false, length = 64)
+    private String oldPassword;
+
+    public UserTennis() {
+    }
+
+    public UserTennis(Long id,  String email, String password, String username, String oldPassword) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.oldPassword = oldPassword;
+    }
+}
